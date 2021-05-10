882c11e5f4cb23f4a66d061eba2f9f29857c201a
desaipriyankaa <priyanka.desai@klingelnberg.com>
2021-05-10 13:40:13 +0530

    Add project files.

---
+++ Even.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EnumerableDemo
+{
+    class Even : IEnumerable<int>
+    {
+        public IEnumerator GetEnumerator()
+        {
+            int i = 0;
+            yield return i;
+            while (true)
+            {
+                i += 2;
+                yield return i;
+            }
+        }
+
+        IEnumerator<int> IEnumerable<int>.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace EnumerableDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Even numbers : ");
+            var even = new Even();
+            foreach (var evnnum in even)
+            {
+                if ((int)evnnum > 50)
+                    continue;
+                Console.WriteLine(evnnum);
+            }
+        }
+    }
+}
